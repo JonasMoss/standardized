7915d437fe4ba5511a23fcaa63353a1a13f88457
Jonas Moss <jonas.gjertsen@gmail.com>
2020-02-29 19:09:58 +0100

    Updated graph.

--- code/graphs.R
+++ code/graphs.R
@@ -44,6 +44,22 @@ plot_errors()
 plot_ordinals = function() {
   pdf("chunks/ordinals.pdf", height = 8, width = 8)
 
+  agreeableness = psychTools::bfi[c("A1", "A2", "A3", "A4", "A5")]
+  agreeableness[, "A1"] = 7 - agreeableness[, "A1"] # Reverse-coded item.
+  poly = psych::polychoric(agreeableness)
+  fa = psych::fa(poly$rho,)
+  lambda = c(fa$loadings)
+  sigma = c(sqrt(fa$uniquenesses))
+
+  outs = c(2:10, 15, 20, 25, 30, 40, 50)
+  ordinal_Hs = sapply(outs, function(out) {
+    ordinal_H(lambda, sigma, cuts = qnorm(seq(0, 1, length.out = out + 1)))
+  })
+
+  ordinal_omegas = sapply(outs, function(out) {
+    ordinal_std(lambda, sigma, cuts = qnorm(seq(0, 1, length.out = out + 1)))
+  })
+
   par(las = 1, mar = c(5.1, 4.1, 4.1, 4.1))
   plot(outs, ordinal_Hs, ylim = c(0.3, 0.8), pch = 19, log = "x",
        xlab = "Levels", ylab = "Reliability")
