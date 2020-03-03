81277b2e809e1153c4e6468f7b55c5914f892a4a
Jonas Moss <jonas.gjertsen@gmail.com>
2020-03-03 15:56:30 +0100

    :+1: Updated graphs for conogive.

--- code/graphs.R
+++ code/graphs.R
@@ -40,37 +40,41 @@ plot_errors()
 
 
 plot_ordinals = function() {
+  if("conogive" %in% rownames(installed.packages()) == FALSE) {
+    stop("You need the package 'congive to run plot_ordinals()")
+  }
+
   pdf("chunks/ordinals.pdf", height = 8, width = 8)
 
   agreeableness = psychTools::bfi[c("A1", "A2", "A3", "A4", "A5")]
   agreeableness[, "A1"] = 7 - agreeableness[, "A1"] # Reverse-coded item.
-  object = latcon(agreeableness)
+  object = conogive::conogive(agreeableness)
 
   outs = c(2:10, 15, 20, 25, 30, 40, 50)
   ordinal_Hs = sapply(outs, function(out) {
     object$cuts = qnorm(seq(0, 1, length.out = out + 1))
-    ordinal_omega(latcon(object), weights = "optimal")
+    conogive::ordinal_omega(conogive::conogive(object), weights = "optimal")
   })
 
-  ordinal_omegas = sapply(outs, function(out) {
+  conogive::ordinal_omegas = sapply(outs, function(out) {
     object$cuts = qnorm(seq(0, 1, length.out = out + 1))
-    ordinal_omega(latcon(object), weights = "equal")
+    conogive::ordinal_omega(conogive::conogive(object), weights = "equal")
   })
 
 
-  omega = ordinal_omega(object, weights = "equal", limit = TRUE)
-  omega_w = ordinal_omega(object, limit = TRUE)
+  omega = conogive::omega(object, weights = "equal")
+  omega_w = conogive::omega(object)
   par(las = 1, mar = c(5.1, 4.1, 4.1, 4.1))
   plot(outs, ordinal_Hs, ylim = c(0.3, 0.8), pch = 19, log = "x",
        xlab = "Levels", ylab = "Reliability")
-  points(outs, ordinal_omegas, pch = 1)
+  points(outs, conogive::ordinal_omegas, pch = 1)
   abline(h = omega, lty = 2)
   abline(h = omega_w)
-  labs = formatC(c(omega, omega_w, ordinal_Hs[5], ordinal_omegas[5]),
+  labs = formatC(c(omega, omega_w, ordinal_Hs[5], conogive::ordinal_omegas[5]),
                  digits = 2)
   abline(v = 6, lty = 3, col = "grey")
-  abline(h = c(ordinal_Hs[5], ordinal_omegas[5]), lty = 3, col = "grey")
-  axis(side = 4, at = c(omega, omega_w,ordinal_Hs[5], ordinal_omegas[5]),
+  abline(h = c(ordinal_Hs[5], conogive::ordinal_omegas[5]), lty = 3, col = "grey")
+  axis(side = 4, at = c(omega, omega_w,ordinal_Hs[5], conogive::ordinal_omegas[5]),
        labels = labs)
   axis(side = 1, 6)
 
